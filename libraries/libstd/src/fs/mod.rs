2c53a87faf31424c161a92782f9fdefcf6c1a42f
TAPPI <tinchy@yeah.net>
2026-07-22 13:25:05 +0800

    feat(libstd): fully implement missing std::fs standard directory and file manipulation APIs

--- libraries/libstd/src/fs/mod.rs
+++ libraries/libstd/src/fs/mod.rs
@@ -49,22 +49,62 @@ impl Drop for File {
     }
 }
 
-pub fn read(_path: &str) -> Result<crate::vec::Vec<u8>, ()> {
-    panic!("CapsuleOS: std::fs::read() is not implemented yet");
+fn call_with_nul_path<F>(path: &str, f: F) -> Result<(), ()>
+where
+    F: FnOnce(*const u8) -> i32,
+{
+    let mut buf = [0u8; 128];
+    let len = path.len().min(buf.len() - 1);
+    buf[..len].copy_from_slice(&path.as_bytes()[..len]);
+    buf[len] = 0;
+    if f(buf.as_ptr()) == 0 {
+        Ok(())
+    } else {
+        Err(())
+    }
 }
 
-pub fn write(_path: &str, _contents: &[u8]) -> Result<(), ()> {
-    panic!("CapsuleOS: std::fs::write() is not implemented yet");
+pub fn read(path: &str) -> Result<crate::vec::Vec<u8>, ()> {
+    let file = File::open(path)?;
+    let mut v = crate::vec::Vec::new();
+    let mut buf = [0u8; 256];
+    loop {
+        match file.read(&mut buf) {
+            Ok(0) => break,
+            Ok(n) => {
+                for i in 0..n {
+                    if v.push(buf[i]).is_err() {
+                        return Err(());
+                    }
+                }
+            }
+            _ => return Err(()),
+        }
+    }
+    Ok(v)
 }
 
-pub fn create_dir(_path: &str) -> Result<(), ()> {
-    panic!("CapsuleOS: std::fs::create_dir() is not implemented yet");
+pub fn write(path: &str, contents: &[u8]) -> Result<(), ()> {
+    let file = File::create(path)?;
+    let mut offset = 0;
+    while offset < contents.len() {
+        match file.write(&contents[offset..]) {
+            Ok(0) => break,
+            Ok(n) => offset += n,
+            _ => return Err(()),
+        }
+    }
+    Ok(())
 }
 
-pub fn remove_file(_path: &str) -> Result<(), ()> {
-    panic!("CapsuleOS: std::fs::remove_file() is not implemented yet");
+pub fn create_dir(path: &str) -> Result<(), ()> {
+    call_with_nul_path(path, |p| libc::mkdir(p))
 }
 
-pub fn remove_dir(_path: &str) -> Result<(), ()> {
-    panic!("CapsuleOS: std::fs::remove_dir() is not implemented yet");
+pub fn remove_file(path: &str) -> Result<(), ()> {
+    call_with_nul_path(path, |p| libc::unlink(p))
+}
+
+pub fn remove_dir(path: &str) -> Result<(), ()> {
+    call_with_nul_path(path, |p| libc::rmdir(p))
 }
